8b630f2d8572830b50a9874c3528dd0c7c8475cd
Jonas Haberkorn <jonas.haberkorn16@gmail.com>
2018-06-21 16:19:41 +0200

    Displaying and actualising every 300ms the module number and the parameters values.

--- Controls/ModuleStatusForm.cs
+++ Controls/ModuleStatusForm.cs
@@ -12,9 +12,88 @@ namespace MissionPlanner.Controls
 {
     public partial class ModuleStatusForm: Form
     {
+        private int NumberParam = 3;
+        private Timer timer1;
+        private IContainer components;
+        private Label ModuleNumber;
+        private List<Label> Param;
+
         public ModuleStatusForm()
         {
             InitializeComponent();
+            Utilities.ThemeManager.ApplyThemeTo(this);
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.SuspendLayout();
+            ModuleStatus tmp = new ModuleStatus(3, new double[]{ 1.2, 1.8, 5 });
+
+            ModuleNumber.Text = "Module : "+tmp.ModuleNumber;
+            if (Param != null)
+            {
+                foreach (var lab in Param)
+                {
+                    this.Controls.Remove(lab);
+                }
+            }
+
+            Param = new List<Label>();
+            int i = 0;
+            foreach (var param in tmp.Param)
+            {
+                Param.Add(new Label());
+                Param[i].AutoSize = true;
+                Param[i].Location = new System.Drawing.Point(71, 25+25*(i+1));
+                Param[i].Name = $"Param {i+1}";
+                Param[i].Size = new System.Drawing.Size(66, 17);
+                Param[i].TabIndex = 0;
+                Param[i].Text = $"Param {i+1} : {param}";
+                this.Controls.Add(Param[i]);
+
+
+                i++;
+            }
+
+            // restore colours
+            Utilities.ThemeManager.ApplyThemeTo(this);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ModuleNumber = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // ModuleNumber
+            // 
+            this.ModuleNumber.AutoSize = true;
+            this.ModuleNumber.Location = new System.Drawing.Point(71, 25);
+            this.ModuleNumber.Name = "ModuleNumber";
+            this.ModuleNumber.Size = new System.Drawing.Size(66, 17);
+            this.ModuleNumber.TabIndex = 0;
+            this.ModuleNumber.Text = "Module : ";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ModuleStatusForm
+            // 
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.ModuleNumber);
+            this.Name = "ModuleStatusForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
     }
 }
